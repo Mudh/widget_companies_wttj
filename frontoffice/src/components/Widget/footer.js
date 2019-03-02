f19679051167ee37c9bebf692918f09a3190879d
John Berry <jonmudh@gmail.com>
2019-03-02 17:17:32 +0100

    Add footer style + hover anim

--- frontoffice/src/components/Widget/footer.js
+++ frontoffice/src/components/Widget/footer.js
@@ -19,6 +19,7 @@ const Footer = () => {
   return (
     <footer className="widget__footer">
       <a
+        className="widget__wttj-link"
         href="https://www.welcometothejungle.co"
         rel="noopener noreferrer"
         target="_blank"
@@ -26,11 +27,19 @@ const Footer = () => {
         <img src={wttjLogo} alt="logo welcome to the jungle" />
       </a>
       <a
+        className="widget__profil-link"
         href="https://www.welcometothejungle.co"
         rel="noopener noreferrer"
         target="_blank"
       >
-        Voir le profil >
+        <span>Voir le profil</span>
+        <svg viewBox="0 0 5.4 10" width="5.384615384615384" height="10">
+          <path
+            d="M0.5,9.1L0.5,9.1c0.2,0.2,0.5,0.2,0.8,0c0,0,0,0,0,0l3.5-3.5c0.3-0.3,0.3-0.8,0-1.1L1.3,0.9c-0.2-0.2-0.5-0.2-0.8,0
+	        c0,0,0,0,0,0l0,0c-0.2,0.2-0.2,0.6,0,0.8l3.1,3.2C3.7,5,3.7,5,3.7,5.1L0.5,8.3C0.3,8.5,0.3,8.9,0.5,9.1z"
+          />
+          />
+        </svg>
       </a>
     </footer>
   );
